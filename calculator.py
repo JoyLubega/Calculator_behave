f05a467b0878eeb9c6948d333123f369810357c9
JoyLubega <jollankent@gmail.com>
2019-01-20 17:35:37 +0300

    Add tests and functionality

---
+++ calculator.py
@@ -0,0 +1,12 @@
+ 
+class Calculator():
+
+    num1 = input('Enter the first number')
+    num2 = input('Enter the second number')
+    
+
+    def add(self, num1, num2):
+        result= num1+num2
+        return (result)
+
+Calculator()
---
+++ steps/steps.py
@@ -0,0 +1,39 @@
+from behave import given, when, then 
+
+from calculator import Calculator
+
+
+
+@given('I have entered {number1:d} into the calculator')
+
+def enter_number1(context, number1):
+    
+
+    context.number1 = number1
+
+
+
+@given('I have also entered {number2:d} into the calculator')
+
+def enter_number2(context, number2):
+
+    context.number2 = number2
+
+
+
+@when('I press add')
+
+def press_add(context):
+    print(context)
+
+    context.calculator = Calculator()
+
+    context.result = context.calculator.add(context.number1, context.number2)
+
+ 
+
+@then('the sum should be {result:d}')
+
+def check_result(context, result):
+
+    assert context.result == result 
